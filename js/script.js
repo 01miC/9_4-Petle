0b0c9c102cadc3637f830597d5bf785928e10829
Michal Dokupil <michal@dokupil.pl>
2017-12-30 00:40:35 +0100

    Add christmas tree with spacer and 'log' option

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-var dinosaur =  'triceratops';
-var dinosaurUpperCase = dinosaur.toUpperCase();
-var textCorrected = text.replace('Velociraptor', dinosaurUpperCase);
-    var half = textCorrected.length/2;
-    var officalText = textCorrected.slice(0, half);
 
-console.log(text);
-console.log('Change the dinosaur with', dinosaur);
-console.log(textCorrected);
-console.log('');
-console.log(officalText);
+
+function drawTree(r) {
+    
+    for (i = 1; i <= r; i++) {
+        var star = ' ';
+        var space = '';
+
+        //spacer
+        for (k=0; k <= r-i; k++) {
+            space += ' ';
+        }
+        //fill
+        for (j = 1; j <= i; j++) {
+            
+            star += '* ';
+            
+        }
+        //display
+        console.log(space+star); 
+        
+        }
+
+        var log = '';
+            for (x=1;x<2; x++) {
+                for (t=1; t <= r-1; t++ ) {
+                    log += ' ';
+                }
+                if (r<=7) {
+                    console.log(log+'Choinka');
+                } else if (r>7 && r<15) {
+                    console.log(log+'|  |');
+                    console.log(log+'Choinka');
+                } else {
+                    console.log(log+'|  |');
+                    console.log(log+'|  |');
+                    console.log(log+'Choinka');
+                }                
+            }
+        
+
+}
+
+var howBig = prompt('Jak duża ma być choinka?');
+drawTree(howBig);
+
